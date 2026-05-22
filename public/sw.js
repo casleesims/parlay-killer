6dbb7428f30959e59bc4048b6d42f79de5c71e8e
Caslee Sims Jr <encrypted@caslees-mbp.myfiosgateway.com>
2026-05-21 21:28:47 -0400

    Revert: roll back to last working state

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'pk-v1';
-const STATIC = ['/', '/manifest.json'];
+const STATIC = ['/', '/math.js', '/manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
